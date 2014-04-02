d8627f8a1bde1294c7ddf7c1b37967105d01e528
daiver <ra22341@ya.ru>
2014-04-02 11:16:54 +0400

    b added

--- 4/main.cpp
+++ 4/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <SWI-Prolog.h>
+#include <iostream>
 
 #define MAXLINE 1024
 
@@ -33,18 +34,39 @@ int main(int argc, char **argv)
         printf("Init succ\n");
        
         term_t h1 = PL_new_term_refs(6);
-        PL_put_integer(h1, 0);
-        PL_put_integer(h1 + 1, 0);
-        PL_put_integer(h1 + 2, 1);
-        PL_put_integer(h1 + 3, 1);
+
+        char ans;
+        printf("Тест на вероятность получения кредита\n");
+        printf("Вы работаете? [y/n]\n");
+        std::cin >> ans;
+        PL_put_integer(h1, ans == 'y');
+        printf("Собираетесь купить бытовую технику, или машину? \
+                [с - машина, t - техника]\n");
+        std::cin >> ans;
+        PL_put_integer(h1 + 1, ans == 'c');
+        printf("Вы мужчина? [y/n]\n");
+        std::cin >> ans;
+        PL_put_integer(h1 + 2, ans == 'y');
+        if (ans)
+            printf("Вы женаты? [y/n]\n");
+        else
+            printf("Вы замужем? [y/n]\n");
+        std::cin >> ans;
+        PL_put_integer(h1 + 3, ans == 'y');
         //pred2 = PL_predicate("tmp2", 3, "user");
         predicate_t pred2 = PL_predicate("classify", 6, "user");
 
         int rval = PL_call_predicate(NULL, PL_Q_NORMAL, pred2, h1);
         char *ch;
-        rval = PL_get_chars(h1 + 4, &ch, CVT_WRITE);
-        printf("res %s\n", ch);
         rval = PL_get_chars(h1 + 5, &ch, CVT_WRITE);
+        printf("res %s\n", ch);
+        rval = PL_get_chars(h1 + 4, &ch, CVT_WRITE);
+        if (strcmp("reject", ch) == 0){
+            printf("К сожалению вам отказано в кредите\n");
+        }
+        else{
+            printf("Поздравляем, вы получили кредит\n");
+        }
         printf("res %s\n", ch);
 
         PL_halt(rval ? 0 : 1);
